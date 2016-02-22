e47752927a21d3a9cd4cdf82da06ed8924b91333
cyy <150614906@qq.com>
2016-02-22 08:04:10 +0800

    new prog

--- tmp/euler66.c
+++ tmp/euler66.c
@@ -7,135 +7,177 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <my_number_theory.h>
+#include <string.h>
 
-#define MAX_D			80
-#define MAX_SQUARE_ROOT 	9999999
+#define MAX_D			200
 
-uint8_t prime_nums[MAX_D+1];
-unsigned __int128 squares[MAX_D+1];
+static unsigned __int128 *squares;
+static uint64_t min_square_root;
 
 int main(int argc,char **argv)
 {
-	uint8_t find_flag;
-	unsigned __int128 D,x,y,y_square,i,next_D_square_root;
-
-	//首先初始化平方数组
-	for(x=1;x<=MAX_D;x++)
-		squares[x]=x*x;
-
-	//接下来我们找出1000以内的质数
-	for(D=2;D<=MAX_D/2;D++)
+	struct
 	{
-		if(prime_nums[D]!=0)
-			continue;
-		for(x=D+D;x<=MAX_D;x+=D)
-			prime_nums[x]=1;
+		uint64_t D;
+		uint64_t max_prime_factor;
+		uint64_t factor_of_prime_factor;
+		uint64_t x;
+		uint64_t y;
+		size_t min_square_root;
+	}*solutions;
+	uint64_t *primes;
+	size_t i,j,square_root,solution_num,max_square_root;
+	uint64_t max_x;
+	__int128 y_square;
+
+	primes=my_primes(MAX_D);
+	if(!primes)
+	{
+		puts("my_primes failed");
+		return -1;
 	}
 
-	next_D_square_root=2;
-	for(D=2;D<=MAX_D;D++)
+	solutions=calloc(MAX_D,sizeof(*solutions));
+	if(!solutions)
 	{
-		//如果D是平方，肯定无解，跳过去
-		if(D==squares[next_D_square_root])
-		{
-			next_D_square_root++;
-			continue;
-		}
-
-		if(D==109)
-			continue;
-
-		//否则的话一定存在，剩下的只是时间问题
-		find_flag=0;
-		y=1;
-
-		//如果D是质数，注意y^2=(x+1)(x-1)/D，那么x+1或者x-1是D的必须是D的倍数，利用这点快速迭代x
-		if(prime_nums[D]==0)	
-		{
-			for(i=1;i;i++)
-			{
-				//考虑x+1的情况，这边i是x+1的倍数
-				y_square=(i*D-2)*i;
-				while(y)
-				{
-					if(y*y==y_square)
-					{
-						find_flag=1;
-						break;
-					}
-					else if(y*y>y_square)
-						break;
-					else 
-						y++;
-				}
-				if(find_flag)
-				{
-					printf("++i=%"PRIu64"\n",(uint64_t)i);
-					printf("++%"PRIu64"^2-%"PRIu64"*%"PRIu64"^2=1\n",(uint64_t)(i*D-1),(uint64_t)D,(uint64_t)y);
-				if(i*D!=(uint64_t)(i*D))
-						puts("截断x");
-				if(y!=(uint64_t)y)
-						puts("截断y");
-					break;
-				}
-				//考虑x-1的情况，这边i是x-1的倍数
-				y_square+=4*i;
-				while(y)
-				{
-					if(y*y==y_square)
-					{
-						find_flag=1;
-						break;
-					}
-					else if(y*y>y_square)
-						break;
-					else 
-						y++;
-				}
-				if(find_flag)
-				{
-					printf("--%"PRIu64"^2-%"PRIu64"*%"PRIu64"^2=1\n",(uint64_t)(i*D+1),(uint64_t)D,(uint64_t)y);
-					break;
-				}
-				else
-					y--;
-			}
-		}
-		else
-		{
-			for(x=2;x;x++)
-			{
-				if((x*x-1)%D!=0)
-					continue;
-				y_square=(x*x-1)/D;
-				while(y)
-				{
-					if(y*y==y_square)
-					{
-						find_flag=1;
-						break;
-					}
-					else if(y*y>y_square)
-						break;
-					else 
-						y++;
-				}
-				if(find_flag)
-					break;
-			}
-
-			if(find_flag)
-			{
-				if(x!=(uint64_t)x)
-						puts("截断x");
-				if(y!=(uint64_t)y)
-						puts("截断y");
-				printf("%"PRIu64"^2-%"PRIu64"*%"PRIu64"^2=1\n",(uint64_t)x,(uint64_t)D,(uint64_t)y);
-			}
-		}
-
-		if(!find_flag)
-			printf("can't find solution for D %"PRIu64,(uint64_t)D);
+		printf("calloc failed:%m");
+		return -1;
 	}
+
+	squares=calloc(100000,sizeof(*squares));
+	if(!squares)
+	{
+		printf("calloc failed:%m");
+		return -1;
+	}
+	min_square_root=1;
+	for(i=min_square_root;i<min_square_root+100000;i++)
+		squares[i-min_square_root]=i*i;
+
+	solution_num=0;
+	square_root=0;
+	for(i=1;i<=MAX_D;i++)
+	{
+		for(j=min_square_root;j<min_square_root+100000;j++)
+		{
+			if(squares[j-min_square_root]>=i)
+				break;
+		}
+		if(squares[j-min_square_root]==i) //跳过平方
+			continue;
+
+		solutions[solution_num].D=i;
+		//找出D的最大素因子
+		for(j=0;primes[j];j++)
+		{
+			if(primes[j]>i)
+				break;
+		}
+		j--;
+		while(j>=0)
+		{
+			if(primes[j]==i || i%primes[j]==0)
+				break;
+			j--;
+		}
+		solutions[solution_num].max_prime_factor=primes[j];
+		solutions[solution_num].factor_of_prime_factor=1;
+		solutions[solution_num].y=1;
+		solution_num++;
+	}
+
+	max_x=3;
+	while(solution_num>1)
+	{
+		for(i=0;i<solution_num;i++)
+		{
+			if(solutions[i].min_square_root!=100000)
+				break;
+		}
+		if(i==solution_num)
+		{
+			min_square_root++;
+			for(j=min_square_root;j<min_square_root+100000;j++)
+				squares[j-min_square_root]=j*j;
+			for(j=0;j<solution_num;j++)
+				solutions[j].min_square_root=0;
+			i=0;
+		}
+
+		//注意y^2=(x+1)(x-1)/D，那么x+1或者x-1是max_prime_factor的倍数，利用这点快速迭代x
+		while(1)
+		{
+			//考虑x+1的情况，这边i是x+1的倍数
+			solutions[i].x=solutions[i].factor_of_prime_factor*solutions[i].max_prime_factor-1;
+			y_square=solutions[i].x*solutions[i].x-1;
+			if(y_square!=0 && (solutions[i].max_prime_factor==solutions[i].D || y_square%solutions[i].D==0))
+			{
+					y_square/=solutions[i].D;
+				if(y_square>squares[99999])
+				{
+					solutions[i].min_square_root=100000;
+					break;
+				}
+				max_square_root=99999;
+				while(solutions[i].min_square_root<=max_square_root)
+				{
+					square_root=(solutions[i].min_square_root+max_square_root)/2;
+					if(squares[square_root]==y_square)
+						break;
+					else if(squares[square_root]<y_square)
+						solutions[i].min_square_root=square_root+1;
+					else
+						max_square_root=square_root-1;
+				}
+
+				if(solutions[i].min_square_root<=max_square_root)
+				{
+					printf("--%"PRIu64"^2-%"PRIu64"*%"PRIu64"^2=1\n",(uint64_t)(solutions[i].x),(uint64_t)solutions[i].D,(uint64_t)square_root+min_square_root);
+					if(solutions[i].x>max_x)
+						max_x=solutions[i].x;
+					memmove(solutions+i,solutions+i+1,(solution_num-i-1)*sizeof(*solutions));
+					solution_num--;
+					break;
+				}
+			}
+
+			//考虑x-1的情况，这边i是x-1的倍数
+			solutions[i].x+=2;
+			y_square=solutions[i].x*solutions[i].x-1;
+			if(solutions[i].max_prime_factor==solutions[i].D || y_square%solutions[i].D==0)
+			{
+					y_square/=solutions[i].D;
+				if(y_square>squares[99999])
+				{
+					solutions[i].min_square_root=100000;
+					break;
+				}
+				max_square_root=99999;
+				while(solutions[i].min_square_root<=max_square_root)
+				{
+					square_root=(solutions[i].min_square_root+max_square_root)/2;
+					if(squares[square_root]==y_square)
+						break;
+					else if(squares[square_root]<y_square)
+						solutions[i].min_square_root=square_root+1;
+					else
+						max_square_root=square_root-1;
+				}
+
+				if(solutions[i].min_square_root<=max_square_root)
+				{
+					if(solutions[i].x>max_x)
+						max_x=solutions[i].x;
+					memmove(solutions+i,solutions+i+1,(solution_num-i-1)*sizeof(*solutions));
+					solution_num--;
+					break;
+				}
+			}
+			solutions[i].factor_of_prime_factor++;
+		}
+	}
+
+	printf("%"PRIu64"\n",solutions[0].D);
 	return 0;
 }
