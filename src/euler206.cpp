4631ef750e9ed0f9754d5bd3066324db10bc68d7
cyyever <cyyever@outlook.com>
2016-03-07 20:05:49 +0800

    完善代码

--- src/euler206.cpp
+++ src/euler206.cpp
@@ -19,88 +19,57 @@ int main(int argc,char **argv)
 	vector<uint64_t> root;
 
 	root=get_root_of_square_form(1);
-	for(auto part_root=begin(root);part_root!=end(root);part_root++)
-	{
-	cout<<*part_root<<endl;
-	}
-	return 0;
 	if(root.size()!=1)
 	{
 		cout<<"get_root_of_square_form failed";
 		return -1;
 	}
 
-	//cout<<root[0]<<endl;
+	cout<<root[0]<<endl;
 	return 0;
 }
 
 /*
- *	功能：获取平方满足模式的正整数
+ *	功能：获取平方满足模式的正整数的模
  *	参数：
  *		digit：平方必须满足digit_(digit+1)_(digit+2)...._0
  *	返回值：
- *		满足条件的正整数的vector，可能是空
+ *		满足条件的正整数的模的vector，可能是空
  */
 vector<uint64_t> get_root_of_square_form(uint8_t digit)
 {
-	uint64_t base,mod,pre_sum,number;
-	ssize_t i,j;
-	uint8_t digit2;
+	uint64_t base,digit_power,mod,pre_sum,number;
+	size_t i;
 	vector<uint64_t> roots,part_roots;
-	char square_str[50];
 
 	mod=1000;
 	for(i=8;i>=digit;i--)
 		mod*=100;
+	base=mod/100;
+	digit_power=base*10;
 
+	//我们先获取平方满足(digit+1)_(digit+2)...._0的数
 	if(digit==9)
-		part_roots.push_back(0);
+		part_roots={10,20,30,40,50,60,70,80,90};
 	else
-	{
 		part_roots=get_root_of_square_form(digit+1);
-		if(part_roots.size()==0)
-			return roots;
-	}
 
 	for(auto part_root=begin(part_roots);part_root!=end(part_roots);part_root++)
 	{
-		base=1;
-		while(base<=*part_root)
-			base*=10;
-
-		pre_sum=0;
-		while(1)
+		//尝试在它前面加上位数
+		for(pre_sum=0;;pre_sum+=base)
 		{
-			number=(pre_sum+*part_root);
-			pre_sum+=base;
-			if(number>mod)
+			if(pre_sum>=mod)
 				break;
+			number=pre_sum+*part_root;
 			//怎么也不能超过Sqrt[1929394959697989990]
 			if(number>1389026623)
 				break;
-			sprintf(square_str,"%" PRIu64,number*number);
-			digit2=0;
-			for(j=strlen(square_str)-1;j>=0;j-=2)
+			if((number*number%mod)/digit_power==digit)
 			{
-				if(square_str[j]!=('0'+digit2))
-				{
-					j=-1;
-					break;
-				}
-				if(digit2==0)
-					digit2=9;
-				else
-				{
-					if(digit2==digit)
-						break;
-					digit2--;
-				}
+				if(find(roots.begin(),roots.end(),number)==roots.end())
+					roots.push_back(number);
 			}
-			if(j<0)
-				continue;
-			
-			if(find(roots.begin(),roots.end(),number)==roots.end())
-				roots.push_back(number);
 		}
 	}
 	return roots;
