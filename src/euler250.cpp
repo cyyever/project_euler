9cb516b68bdc1377de1c0c66128726c396fc4067
cyy <cyyever@outlook.com>
2017-05-10 18:56:03 +0800

    优化代码

--- src/euler250.cpp
+++ src/euler250.cpp
@@ -5,8 +5,8 @@
  */
 
 #include <algorithm>
-#include <iostream>
 #include <array>
+#include <iostream>
 
 /*
  *	功能：计算(base^power)%modulus
@@ -24,13 +24,13 @@ static uint64_t power(uint64_t base, uint64_t power, uint64_t modulus) {
   while (power) {
     if (power & 1) {
       res *= tmp_base;
-      if (modulus&& res >= modulus) {
+      if (modulus && res >= modulus) {
         res %= modulus;
       }
     }
     power >>= 1;
     tmp_base *= tmp_base;
-    if (modulus&& tmp_base >= modulus) {
+    if (modulus && tmp_base >= modulus) {
       tmp_base %= modulus;
     }
   }
@@ -38,35 +38,31 @@ static uint64_t power(uint64_t base, uint64_t power, uint64_t modulus) {
 }
 
 int main(void) {
-  std::array<uint64_t,251> sum{};
+  std::array<uint64_t, 250> sum{};
+  std::array<uint64_t, 250> next_sum{};
 
-  uint64_t result_modulus=power(10,16,0);
+  uint64_t result_modulus = power(10, 16, 0);
 
   for (uint64_t i = 1; i <= 250250; i++) {
-    auto res=power(i, i,250);
-    if(res==0) {
-      res=250;
-    }
+    auto res = power(i, i, 250);
 
-    auto next_sum=sum;
-
-    for(size_t j=1;j<=250;j++) {
-      auto k=j+res;
-      if(k>250) {
-	k-=250;
+    for (size_t j = 0; j < 250; j++) {
+      auto k = j + res;
+      if (k >= 250) {
+        k -= 250;
       }
-      next_sum[k]=sum[j];
+      next_sum[k] = sum[j];
     }
 
-    for(size_t j=1;j<=250;j++) {
-      sum[j]+=next_sum[j];
-      if(sum[j]>=result_modulus) {
-	sum[j]%=result_modulus;
-      }
-    }
     sum[res]++;
+    for (size_t j = 0; j < 250; j++) {
+      sum[j] += next_sum[j];
+      if (sum[j] >= result_modulus) {
+        sum[j] -= result_modulus;
+      }
+    }
   }
 
-  std::cout <<sum[250]<< std::endl;
+  std::cout << sum[0] << std::endl;
   return 0;
 }
