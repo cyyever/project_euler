ded80505ec867045475b22160e2a808b8ea1d2e0
cyy <cyyever@outlook.com>
2019-11-11 18:18:58 +0800

    refactor code

--- src/euler65.cpp
+++ src/euler65.cpp
@@ -4,13 +4,12 @@
  *	功能：解决eulerproject 65题(https://projecteuler.net/problem=65)
  */
 
-#include <cyy/math/my_math.h>
+#include <cyy/math/integer.hpp>
 #include <iostream>
 
-using namespace my_math;
 using namespace std;
 
-int get_con_frac_element(size_t n) {
+static uint64_t get_con_frac_element(uint64_t n) {
   if (n == 1) {
     return 2;
   } else if ((n - 1) % 3 != 2) {
@@ -21,22 +20,19 @@ int get_con_frac_element(size_t n) {
 }
 
 int main() {
-
-  my_int n, d;
+  cyy::math::integer n, d;
 
   n = get_con_frac_element(100);
   d = 1;
-  for (size_t i = 99; i >= 1; i--) {
+  for (uint64_t i = 99; i >= 1; i--) {
     auto tmp = n;
     n = n * get_con_frac_element(i) + d;
-    d = tmp;
+    d = std::move(tmp);
   }
 
   //可以证明n和d是互质的，所以没必要简化分数
-  std::string n_str = n;
-
   size_t sum = 0;
-  for (const auto &c : n_str) {
+  for (const auto &c :n.to_string()) {
     sum += c - '0';
   }
 
