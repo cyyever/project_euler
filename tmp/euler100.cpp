56361bfd36ee17014740927d5533dbd9235fb6c1
cyy <cyyever@outlook.com>
2021-11-15 20:09:29 +0800

    fix code

--- tmp/euler100.cpp
+++ tmp/euler100.cpp
@@ -5,26 +5,71 @@
  */
 
 #include <algorithm>
-#include <cyy/math/all.hpp>
 #include <iostream>
 #include <utility>
 
-int main(void) {
+#include <cyy/math/all.hpp>
+
+static cyy::math::integer find_blue(const cyy::math::integer &total_number) {
+  // find the smallest blue such that 2*blue*(blue-1)>=total(total_number-1)
+
+  cyy::math::integer lower = 1;
+  cyy::math::integer upper = total_number;
+  cyy::math::integer limit = total_number * (total_number - 1);
+  while (lower <= upper) {
+    auto mid = (lower + upper) / 2;
+    auto res = (mid * (mid - 1) * 2) <=> limit;
+    if (res == std::strong_ordering::equal) {
+      return mid;
+    }
+    if (res == std::strong_ordering::less) {
+      lower = mid + 1;
+    } else {
+      upper = mid - 1;
+    }
+  }
+  assert(lower * 2 * (lower - 1) > limit);
+  --lower;
+  assert(lower * 2 * (lower - 1) < limit);
+  return lower;
+}
+/* static std::pair<uint64_t, cyy::math::integer> */
+/* increase_blue(uint64_t blue, cyy::math::integer error) { */
+/*   assert(error)>0; */
+/*   cyy::math::rational =error /= 2; */
+/*   cyy::math::integer B = blue * 2 - 1; */
+/*   while(lower<=upper) { */
+/*     auto mid=(lower+upper)/2; */
+
+/*     if(mid) */
+/*     if(lower<=up) */
+/*   } */
+
+/*   auto C = -std::move(error); */
+
+/*   return {}; */
+/* } */
+
+int main() {
   uint64_t n = 1000000000000;
 
+  auto blue = find_blue(n);
   auto total = cyy::math::integer(n) * (n - 1);
-  auto a = static_cast<uint64_t>(n / 1.414);
-  auto lower = cyy::math::integer(a) * ((a - 1) * 2);
+  auto lower = cyy::math::integer(blue) * ((blue - 1) * 2);
+
+  assert((total - lower) >= 0);
+  // we need to adjust lower
   while (true) {
-    auto res = lower.compare(total);
-    if (res < 0) {
-      lower += 4 * a;
-      a++;
+    auto res = (total - lower);
+    std::cout << "res=" << res.to_string() << std::endl;
+    if (res > 0) {
+      lower += blue * 4;
+      blue++;
     } else if (res == 0) {
-      std::cout << a << std::endl;
+      std::cout << blue << std::endl;
       return 0;
     } else {
-      total += 2 * n;
+      total += n * 2;
       n++;
     }
   }
